e2b29678eb017893af1060b57bb69449b410fad9
marek <marek.jastrzebski@outlook.com>
2020-03-18 22:43:34 +0100

    external data source and csv data parser added

---
+++ src/main/java/com/mj/geolocation/CovidDataParser.java
@@ -0,0 +1,48 @@
+package com.mj.geolocation;
+
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.http.client.HttpClient;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class CovidDataParser {
+    public static final String COVID_CONFIRMED_URL=
+            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public List<Point> getCovidData() throws IOException {
+        List<Point> points=new ArrayList<>();
+
+        RestTemplate restTemplate=new RestTemplate();
+        String responseString=restTemplate.getForObject(COVID_CONFIRMED_URL, String.class);
+//        System.out.println(responseString);
+
+        StringReader stringReader=new StringReader(responseString);
+        CSVParser csvParser=CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
+
+        for(CSVRecord record:csvParser){
+            double lat=Double.parseDouble(record.get("Lat"));
+            double lon=Double.parseDouble(record.get("Long"));
+            String infected=record.get("3/16/20");
+            points.add(new Point(lat,lon, infected));
+        }
+        return points;
+    }
+}
+
--- src/main/java/com/mj/geolocation/MapController.java
+++ src/main/java/com/mj/geolocation/MapController.java
@@ -5,21 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MapController {
+    private CovidDataParser covidDataParser;
 
-    @GetMapping
+    public MapController(CovidDataParser covidDataParser) {
+        this.covidDataParser = covidDataParser;
+    }
+
+    @GetMapping("/1")
     public String getSingleMarker(Model model, @RequestParam  String x, @RequestParam String y){
         model.addAttribute("x", x);
         model.addAttribute("y", y);
         return "map";
     }
 
-    @GetMapping("/full")
-    public String getMarkerList(Model model){
+    @GetMapping("/2")
+    public String getTwoMarkers(Model model){
         List<Point> pointList= new ArrayList<>();
         pointList.add(new Point(51.76, 19.48, "Wykryte przypadki: 1"));
         pointList.add(new Point(50.82, 17.14, "Wykryte przypadki: 2"));
@@ -27,5 +33,12 @@ public class MapController {
         return "map2";
     }
 
+    @GetMapping("/all")
+    public String getAllMakers(Model model) throws IOException {
+
+        List<Point> points=covidDataParser.getCovidData();
+        model.addAttribute("pointList", points);
+        return "mapAll";
+    }
 
 }
